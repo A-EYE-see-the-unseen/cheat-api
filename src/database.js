15988b75054993945a61468e0c1a9861dca3f338
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-05-21 01:43:08 +0700

    [create] - configure database

---
+++ src/database.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const Connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "cheating",
+});
+
+Connection.connect(function (err) {
+  if (err) {
+    console.log(`Error in part ${err}`);
+  }
+  console.log("Database connected!");
+});
+
+module.exports = Connection;
