f6f286f21be16132244aac1754f507de085979a2
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-05-31 16:44:28 +0700

    [update]-add-cookies and cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const router = require("./routes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
@@ -8,12 +9,18 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["*"],
+    methods: ["POST", "GET"],
+  })
+);
 
 app.use("/api", router);
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
