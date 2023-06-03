53db5abb6c0fa1df581ac0b345a66c827759b615
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-06-03 13:54:18 +0700

    [update]-configure prod db

--- src/database.js
+++ src/database.js
@@ -1,16 +1,16 @@
 const mysql = require("mysql");
 const Connection = mysql.createConnection({
-  host: "localhost",
+  host: "34.128.124.77",
   user: "root",
-  password: "",
+  password: "12345",
   database: "cheating",
   port: 3306,
-  multipleStatements: true,
+  //   multipleStatements: true,
 });
 
 Connection.connect(function (err) {
   if (err) {
-    console.log("error");
+    console.log("error in ");
   } else {
     console.log("Database connected!");
   }
